99bfab559ecef8edd45dd014181bd459a5b575f5
Lucas Ortis <lucasortis01@gmail.com>
2021-09-21 19:35:26 -0300

    Initial project

---
+++ WorkingWithFiles3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFiles3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Lucas Ortis\Downloads\file1.txt";
+
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            Console.WriteLine(sr.ReadLine());
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred " + e.Message);
+            }
+
+            string path2 = @"C:\Users\Lucas Ortis\Downloads\file2.txt";
+
+            try
+            {
+                using (StreamReader sr2 = File.OpenText(path2))
+                {
+                    while (!sr2.EndOfStream)
+                    {
+                        Console.WriteLine(sr2.ReadLine());
+                    }
+                }
+            } 
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred " + e.Message);
+            }
+
+        }
+    }
+}
